d4a605c2ebe646b92fbebc46e4f1db4e43c62f27
Albert Garangou Culebras <albertgarangou@gmail.com>
2020-10-01 16:31:04 +0200

    Update and rename Twitter per Calçot intel·ligent.py to Twitter_per_Calçot_intel_ligent.py

--- Twitter_per_Calçot_intel_ligent.py
+++ Twitter_per_Calçot_intel_ligent.py
@@ -6,6 +6,5 @@ auth = tweepy.OAuthHandler('API-KEY-HERE', 'API-PASSWORD-KEY-HERE')
 auth.set_access_token('TOKEN-KEY-HERE', 'TOKEN-PASSWORD-KEY-HERE')
 api = tweepy.API(auth)
 
-
 message = input("tweet: ")
 api.update_status(status=message, place_id='Calçot City')
